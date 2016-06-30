b53744213d628de38af5c99caecb68b2c37e7bcf
Zart Colwing <zartc@wanadoo.fr>
2016-06-30 22:45:27 +0200

    initial commit

---
+++ src/main/java/com/zc/study/fileinjection/FileInjectionApplication.java
@@ -0,0 +1,12 @@
+package com.zc.study.fileinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileInjectionApplication.class, args);
+	}
+}
